72a8e813f706864736f8807cb493b47e7a218ef8
Abolfazl <aboooooo57@outlook.com>
2024-04-01 00:16:04 +0330

    get file from minio tested

--- upload_file.py
+++ upload_file.py
@@ -1,19 +1,33 @@
 import io
 
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, File, UploadFile, HTTPException, Response
+from fastapi.responses import JSONResponse, HTMLResponse
+from minio.versioningconfig import VersioningConfig
+
 from minio_conf import MinioClient as minioobj
-from settings import MINIO_BUCKET_NAME, MINIO_FILE_PATH
+from settings import MINIO_BUCKET_NAME, MINIO_FILE_PATH, CRYPTOGRAPHY_KEY, HOST
 from hashlib import sha512
 from datetime import datetime
 import magic
 from io import BytesIO
+from cryptography.fernet import Fernet
+from minio.error import InvalidResponseError
 
 app = FastAPI()
 
 
 def hash_file_name(file_name) -> str:
     return sha512((file_name + str(datetime.now())).encode()).hexdigest()
+
+
+def encrypt_version_id(version_id) -> str:
+    f = Fernet(bytes(CRYPTOGRAPHY_KEY, 'utf-8'))
+    return f.encrypt(str(version_id).encode()).decode()
+
+
+def decrypt_version_id(version_id) -> str:
+    f = Fernet(bytes(CRYPTOGRAPHY_KEY, 'utf-8'))
+    return f.decrypt(str(version_id).encode()).decode()
 
 
 def is_mp3(buffer: BytesIO) -> bool:
@@ -25,111 +39,55 @@ def is_mp3(buffer: BytesIO) -> bool:
 
 
 async def save_file_to_minio(file: UploadFile):
-    # Set your Minio bucket name
     bucket_name = MINIO_BUCKET_NAME
-
-    # Initialize BytesIO buffer
     buffer = BytesIO()
-
-    # Write the file data to BytesIO buffer
     while True:
         chunk = await file.read(1024)  # Read 1 KB at a time
         if not chunk:
             break
         buffer.write(chunk)
 
-    # Reset buffer position to start
-    hashed_file_name = hash_file_name(file.filename)
-    object_name = hashed_file_name
-
     if not is_mp3(buffer):
         raise HTTPException(status_code=400, detail="Uploaded file is not an MP3")
 
     buffer.seek(0)
-
-    # Upload the file to Minio
-    minioobj.get_client().client.put_object(
+    minioobj.get_client().client.set_bucket_versioning(
         bucket_name,
-        object_name + file.filename,
+        VersioningConfig(status="Enabled")
+    )
+    print(file.content_type)
+    now = datetime.now().strftime("%Y%m%d%H%M%S%f")
+    res = minioobj.get_client().client.put_object(
+        bucket_name,
+        now,
         buffer,
-        len(buffer.getvalue()),  # Specify the size of the data in the buffer
-        content_type=file.content_type  # Set the content type
+        len(buffer.getvalue()),
+        content_type=file.content_type
     )
     print(len(buffer.getvalue()))
-
-
-# async def save_file_to_minio(file: UploadFile):
-#     # Set your Minio bucket name
-#     bucket_name = MINIO_BUCKET_NAME
-#
-#     # Set the buffer size for streaming (adjust as needed)
-#     buffer_size = 1024 * 1024  # 1 MB
-#
-#     # Initialize variables
-#     total_bytes_written = 0
-#
-#     # Write the file data to Minio in chunks
-#     with minioobj.get_client().client.put_object(bucket_name, file.filename, file.file, file.size,
-#                                                  content_type=file.content_type) as result:
-#         # Iterate over the file chunks and write them to Minio
-#         while True:
-#             chunk = await file.read(buffer_size)
-#             if not chunk:
-#                 break
-#             total_bytes_written += len(chunk)
-#             result.write(chunk)
-#
-#     # Print the total bytes written (optional)
-#     print(f"Total bytes written: {total_bytes_written}")
+    # buffer.close()
+    return now, res.version_id
 
 
 @app.post("/uploadfile/")
 async def upload_file(file: UploadFile = File(...)):
-    await save_file_to_minio(file)
-    return {"filename": file.filename, "message": "File uploaded successfully to Minio!"}
-#
-# @app.post("/uploadfile/")
-# async def upload_file(file: UploadFile = File(...)):
-#
-#     contents = await file.read()
-#     if not is_mp3(contents):
-#         raise HTTPException(status_code=400, detail="Uploaded file is not an MP3")
-#
-#     minio_client = minioobj.get_client().client
-#     hashed_file_name = hash_file_name(file.filename)
-#     object_name = hashed_file_name
-#
-#     # Read file data
-#     file_data = await file.read()
-#     file_size = file.size
-#
-#     print(dir(file))
-#     print(dir(file.file),"filesize")
-#     print(type(file.file),"readdd")
-#     print(dir(file))
-#     print(file.size)
-#     response = minio_client.put_object(
-#             MINIO_BUCKET_NAME,
-#             object_name,
-#             io.BytesIO(file_data),
-#             file_size,
-#             content_type=file.content_type
-#     )
-#     print(dir(response))
-#     print(response.location)
-#     print(response.object_name)
-#     print(response.bucket_name)
-#     return {"message": "File uploaded successfully"}
-#     # Ensure that the object is uploaded successfully
-# if response.status == 200:
-#
-# else:
-#     return {"message": "Failed to upload file"}
+    object_name, version_id = await save_file_to_minio(file)
+    print(version_id, "id")
+    print(encrypt_version_id(version_id), "encrypted version_id")
+    return {
+        "link": HOST + f"/listen/{encrypt_version_id(version_id)}" + ":" + f"{object_name}",
+        "filename": file.filename,
+        "message": "File uploaded successfully!"}
 
 
-# print(file,"file")
-# print(dir(file),"dirfile")
-# print(dir(file.file))
-# minio_client.upload_file(bucket_name=MINIO_BUCKET_NAME, object_name=hashed_file_name,file_path=file)
-#
-# return JSONResponse(content={"filename": hashed_file_name, "status": "uploaded"})
+@app.get("/listen/{token}")
+async def listen(token: str):
+    try:
+        version_id = token.split(":")[0]
+        object_name = token.split(":")[-1]
+        with minioobj.get_client().client.get_object(bucket_name=MINIO_BUCKET_NAME, object_name=object_name,
+                                                     version_id=decrypt_version_id(version_id)) as file_data:
+            html_content = file_data.read().decode()
+            return HTMLResponse(content=html_content)
+    except InvalidResponseError as err:
+            return {"error": f"Failed to retrieve file: {err}"}
