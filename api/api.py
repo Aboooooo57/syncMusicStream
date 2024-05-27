3ccf4b407ed1658b273495a0c275d4e1c6dd46cf
Abolfazl <aboooooo57@outlook.com>
2024-05-27 11:31:10 +0330

    sse added but not working

--- api/api.py
+++ api/api.py
@@ -1,7 +1,10 @@
+import asyncio
+import json
 import os
 
 from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.responses import HTMLResponse, Response
+from starlette.responses import JSONResponse
 
 from minio_conf import MinioClient
 from settings import MINIO_BUCKET_NAME, HOST
@@ -11,10 +14,12 @@ from minio.error import InvalidResponseError
 from utils import decrypt_version_id, encrypt_version_id
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from sse_starlette.sse import EventSourceResponse
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="templates"), name="static")
+
 
 def hash_file_name(file_name) -> str:
     return sha256((file_name + str(datetime.now())).encode()).hexdigest()
@@ -24,6 +29,7 @@ def generate_cookie_value():
     random_bytes = os.urandom(32)
     hash_object = sha256(random_bytes)
     return hash_object.hexdigest()
+
 
 @app.get("/upload")
 async def upload():
@@ -44,33 +50,111 @@ async def upload_file(file: UploadFile = File(...)):
         "message": "File uploaded successfully!"}
 
 
+event = {}
+connected_clients = {}
+
+
 def check_cookie(request: Request, response: Response):
-    if not request.cookies.get("usr"):
-        response.set_cookie(key="usr", value=generate_cookie_value())
-    return response
+    usr_cookie = request.cookies.get("usr")
+    if not usr_cookie:
+        usr_cookie = generate_cookie_value()
+        response.set_cookie(key="usr", value=usr_cookie)
+    # connected_clients.update({"usr": request.cookies.get("usr")})
+    return response, usr_cookie
+
+
+def init_connected_client(usr_cookie, token):
+    # print(token,"token init"*50)
+    connected_clients[usr_cookie] = {"client": "", "token": token, "PlayOrPause": False}
+    # print(connected_clients)
 
 
 @app.get("/listen/{token}")
 async def listen(request: Request, token: str):
     try:
         link = f"http://{HOST}/stream/{token}"
-        html_content = templates.get_template("index.html").render(mp3_url=link,request=request)
+        html_content = templates.get_template("index.html").render(mp3_url=link, request=request)
         response = HTMLResponse(content=html_content)
-        response = check_cookie(request, response)
-
+        response, usr_cookie = check_cookie(request, response)
+        init_connected_client(usr_cookie, token)
         return response
     except InvalidResponseError as err:
         return {"error": f"Failed to retrieve file: {err}"}
 
 
 @app.get("/stream/{token}")
-async def stream(token: str):
+async def stream(request: Request, token: str):
     try:
         version_id = token.split(":")[0]
         object_name = token.split(":")[-1]
+
         with MinioClient.get_instance().client.get_object(bucket_name=MINIO_BUCKET_NAME, object_name=object_name,
                                                           version_id=decrypt_version_id(version_id)) as file_data:
             response = Response(content=file_data.read(), media_type="audio/mpeg")
             return response
+
     except InvalidResponseError as err:
         return {"error": f"Failed to retrieve file {err}"}
+
+
+async def broadcast_pause(usr, token):
+    for other_usr, client_data in connected_clients.items():
+        if other_usr != usr and client_data["token"] == token and client_data["PlayOrPause"]:
+            await send_pause_event(other_usr)
+
+
+def broadcast_play(usr, token):
+    for other_usr, client_data in connected_clients.items():
+        if not other_usr == usr and client_data["token"] == token and not client_data["PlayOrPause"]:
+            send_play_event(other_usr)
+
+
+async def send_pause_event(usr):
+    client = connected_clients[usr].get("client")
+    global event
+    if client:
+        # print("sending pause event")
+        event = {"event": "pause"}
+        # await client.send_event("pause", data={"message": "Music paused"})
+    # print("error_pause_event", usr)
+
+
+def send_play_event(usr):
+    client = connected_clients[usr].get("client")
+    global event
+    if client:
+        event = {"event": "play"}
+        # client.send_event("play", data={"message": "Music played"})
+    # print("error_play_event", usr)
+
+
+@app.get("/music/play/{token}")
+async def play(request: Request, token: str):
+    usr = request.cookies.get("usr")
+    connected_clients[usr]["PlayOrPause"] = True
+    broadcast_play(usr, token)
+    return JSONResponse(status_code=200, content={"message": ""})
+
+
+@app.get("/music/pause/{token}")
+async def pause(request: Request, token: str):
+    usr = request.cookies.get("usr")
+    connected_clients[usr]["PlayOrPause"] = False
+    await broadcast_pause(usr, token)
+    return JSONResponse(status_code=200, content={"message": ""})
+
+
+async def event_generator():
+    global event
+    while True:
+        if event != {}:
+            yield json.dumps(event)
+            event = {}
+        await asyncio.sleep(1)  # Example: generate events every second
+
+
+@app.get("/sse")
+async def sse_endpoint(request: Request):
+    client_id = request.cookies.get("usr")
+    connected_clients[client_id]["client"] = request.scope["client"]
+    return EventSourceResponse(event_generator())
--- api/templates/js/js.js
+++ api/templates/js/js.js
@@ -1,98 +1,144 @@
-var websocket = new WebSocket("ws://185.230.162.51:8002");
+// var eventSource = new EventSource("http://185.230.162.51:8002/sse"); // Establish SSE connection
 var audioPlayer = document.getElementById("audioPlayer");
-var locallyInitiatedPlay = false;
-const deviceId = getCookie("usr");
-var callerId = deviceId;
-var seeking = false;
+// var locallyInitiatedPlay = false;
+// const deviceId = getCookie("usr");
+// var callerId = deviceId;
+// var seeking = false;
+//
+var currentUrl = window.location.href;
+var parsedUrl = new URL(currentUrl);
+var token = parsedUrl.pathname.split('/').pop();
 
-websocket.onmessage = function(event) {
-    var message = event.data;
-    var parts = message.split(":");
-    var recDeviceId = parts[1];
-    if (message.startsWith("playing:") || message.startsWith("paused:")) {
 
-        if (parseInt(recDeviceId) !== parseInt(deviceId)) {
-            callerId = recDeviceId;
-            locallyInitiatedPlay = true;
-            if (message.startsWith("playing:")) {
-                console.log("Received play command from device:", recDeviceId);
-                audioPlayer.play();
-            } else {
-                console.log("Received paused command from device:", recDeviceId);
-                audioPlayer.pause();
-            }
-        }
+// Function to handle SSE events
+function handleEvent(event) {
+    const data = JSON.parse(event.data);
+    console.log(data);
+    if (data.event === "pause") {
+        audioPlayer.pause();
+    } else if (data.event === "play") {
+        audioPlayer.play();
     }
-    else if (message.startsWith("position_update:")){
-        var deviceId = parts[1];
-        var position = parseFloat(parts[2]);
-        console.log("Received position update from device:", deviceId, "Position:", position);
-        if (!isNaN(position)) {
-            console.log("i'm here")
-            audioPlayer.currentTime = position;
-        }
-    }
-};
+}
 
+// Function to establish SSE connection
+function connectSSE() {
+    const eventSource = new EventSource("/sse");
+
+    // Add event listener to handle incoming events
+    eventSource.addEventListener("message", handleEvent);
+
+    // Add event listener to handle connection errors
+    eventSource.addEventListener("error", function (event) {
+        console.error("SSE connection error:", event);
+    });
+}
+
+// Call the connectSSE function when the page loads
+window.onload = connectSSE;
+
+
+// eventSource.onmessage = function(event) {
+//     var message = event.data;
+//     var parts = message.split(":");
+//     var recDeviceId = parts[1];
+//     if (message.startsWith("playing:") || message.startsWith("paused:")) {
+//         if (parseInt(recDeviceId) !== parseInt(deviceId)) {
+//             callerId = recDeviceId;
+//             locallyInitiatedPlay = true;
+//             if (message.startsWith("playing:")) {
+//                 console.log("Received play command from device:", recDeviceId);
+//                 audioPlayer.play();
+//             } else {
+//                 console.log("Received paused command from device:", recDeviceId);
+//                 audioPlayer.pause();
+//             }
+//         }
+//     } else if (message.startsWith("position_update:")) {
+//         var deviceId = parts[1];
+//         var position = parseFloat(parts[2]);
+//         console.log("Received position update from device:", deviceId, "Position:", position);
+//         if (!isNaN(position)) {
+//             console.log("i'm here")
+//             audioPlayer.currentTime = position;
+//         }
+//     }
+// };
+//
 audioPlayer.addEventListener("play", function() {
-    if (websocket.readyState === WebSocket.OPEN) {
-        if (!locallyInitiatedPlay || parseInt(callerId) !== parseInt(deviceId)) {
-            if (!seeking && deviceId) {
-                var message = "PLAY:" + deviceId;
-                websocket.send(message);
-                console.log("Sent play command to all devices.");
-            }
-        }
-        locallyInitiatedPlay = false;
-    }
-});
 
+    fetch('/music/play/'+ token)
+        .then(response => {
+            console.log(response);
+            if (response.status === 200) {
+                console.log("Play command sent successfully.");
+            } else {
+                console.error("Failed to send play command:", response.status);
+            }
+        })
+        .catch(error => {
+            console.error("Error sending play command:", error);
+        });
+    // if (!locallyInitiatedPlay || parseInt(callerId) !== parseInt(deviceId)) {
+    //     if (!seeking && deviceId) {
+    //         var message = "PLAY:" + deviceId;
+    //         eventSource.send(message);
+    //         console.log("Sent play command to all devices.");
+    //     }
+    // }
+    // locallyInitiatedPlay = false;
+});
+//
 audioPlayer.addEventListener("pause", function() {
-    if (websocket.readyState === WebSocket.OPEN) {
-        if (!locallyInitiatedPlay || parseInt(callerId) !== parseInt(deviceId)) {
-            if (!seeking && deviceId) {
-                var message = "PAUSE:" + deviceId;
-                websocket.send(message);
-                console.log("Sent pause command to all devices.");
-            }
-        }
-        locallyInitiatedPlay = false;
-    }
-});
+    fetch('/music/pause/'+token)
+    .then(response => {
+        console.log(response);
 
-audioPlayer.addEventListener("seeking", function() {
-    seeking = true;
-});
-
-audioPlayer.addEventListener("seeked", function() {
-    seeking = false;
-});
-
-
-
-
-
-function syncMusic(){
-    var position = audioPlayer.currentTime;
-    var message = "UPDATE_POSITION:" + deviceId + ":" + position;
-    websocket.send(message);
-    console.log("Updated position for device:", deviceId, "Position:", position);
-
-}
-function registerDevice() {
-    if (websocket.readyState === WebSocket.OPEN) {
-        if (deviceId) {
-            var message = "REGISTER_DEVICE:" + deviceId;
-            websocket.send(message);
-            console.log("Registered device:", deviceId);
+        if (response.status === 200) {
+            console.log("pause command sent successfully.");
         } else {
-            console.log("Please provide a device ID.");
+            console.error("Failed to send pause command:", response.status);
         }
-    } else {
-        console.log("WebSocket connection not open.");
-    }
-}
+    })
+    .catch(error => {
+        console.error("Error sending pause command:", error);
+    });
 
+    // if (!locallyInitiatedPlay || parseInt(callerId) !== parseInt(deviceId)) {
+    //     if (!seeking && deviceId) {
+    //         var message = "PAUSE:" + deviceId;
+    //         eventSource.send(message);
+    //         console.log("Sent pause command to all devices.");
+    //     }
+    // }
+    // locallyInitiatedPlay = false;
+});
+//
+// audioPlayer.addEventListener("seeking", function() {
+//     seeking = true;
+// });
+//
+// audioPlayer.addEventListener("seeked", function() {
+//     seeking = false;
+// });
+//
+// function syncMusic() {
+//     var position = audioPlayer.currentTime;
+//     var message = "UPDATE_POSITION:" + deviceId + ":" + position;
+//     eventSource.send(message);
+//     console.log("Updated position for device:", deviceId, "Position:", position);
+// }
+//
+// function registerDevice() {
+//     if (deviceId) {
+//         var message = "REGISTER_DEVICE:" + deviceId;
+//         eventSource.send(message);
+//         console.log("Registered device:", deviceId);
+//     } else {
+//         console.log("Please provide a device ID.");
+//     }
+// }
+//
 function getCookie(name) {
     const cookies = document.cookie.split(';');
     for (let cookie of cookies) {
@@ -103,22 +149,8 @@ function getCookie(name) {
     }
     return null;
 }
-
-document.addEventListener("DOMContentLoaded", function(event) {
-
-    websocket.onopen = function(event) {
-        console.log("WebSocket connection established.");
-        registerDevice();
-    };
-
-
-
-websocket.onerror = function(event) {
-    console.error("WebSocket error:", event);
-};
-
-websocket.onclose = function(event) {
-    console.log("WebSocket connection closed.");
-};
-
-});
+//
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     console.log("DOMContentLoaded event fired.");
+//     registerDevice();
+// });
